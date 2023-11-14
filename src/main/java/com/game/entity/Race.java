e14625beac3b05fedc3ed31fe81c0eefff1fb840
Devilases <zhelonin.2@yandex.ru>
2023-11-14 19:58:27 +0300

    1

---
+++ src/main/java/com/game/entity/Race.java
@@ -0,0 +1,16 @@
+package com.game.entity;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+
+public enum Race {
+
+    HUMAN,
+    DWARF,
+    ELF,
+    GIANT,
+    ORC,
+    TROLL,
+    HOBBIT
+}
---
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -0,0 +1,61 @@
+package com.game.repository;
+
+import com.game.entity.Player;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import org.springframework.stereotype.Repository;
+import javax.annotation.PreDestroy;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO;
+
+
+@Repository(value = "db")
+public class PlayerRepositoryDB implements IPlayerRepository {
+
+
+
+
+    public PlayerRepositoryDB() {
+
+
+    }
+
+    @Override
+    public List<Player> getAll(int pageNumber, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public int getAllCount() {
+        return 0;
+    }
+
+    @Override
+    public Player save(Player player) {
+        return null;
+    }
+
+    @Override
+    public Player update(Player player) {
+        return null;
+    }
+
+    @Override
+    public Optional<Player> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(Player player) {
+
+    }
+
+    @PreDestroy
+    public void beforeStop() {
+
+    }
+}
